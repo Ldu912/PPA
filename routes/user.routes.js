e3931208a874b22b77a63b5f9ef7ac2272f4276b
Ouzidane Alexis <ouzidanealexis@MacBook-Pro-de-admin.home>
2021-07-13 00:33:43 +0200

    recuperation des users + leurs infos

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,7 +1,11 @@
 const router = require("express").Router();
 const authController = require("../controllers/auth.controller");
+const userController = require("../controllers/user.controller");
 
 // auth
 router.post("/register", authController.signUp);
+
+router.get("/", userController.getAllUsers);
+router.get("/:id", userController.userInfo);
 
 module.exports = router;

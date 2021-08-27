114ea1762c141cdcd26b7be88802be0e21ce16b2
Alexis AOE. OUZIDANE <AOUZIDANE@ASREC.LAN>
2021-08-27 16:26:29 +0200

    front sign in et sign up ok

---
+++ client/src/pages/Home.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Profil = () => {
+  return <div>Yo la famille</div>;
+};
+
+export default Profil;
---
+++ client/src/pages/Trending.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Trending = () => {
+    return (
+        <div>
+            Yo c'est les tendances fraté
+        </div>
+    );
+};
+
+export default Trending;
